784e5bde5d4990a41712b301ece74cc9f43cc46e
atifpv11 <115897151+atifpv11@users.noreply.github.com>
2024-10-03 11:28:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let a = str.charAt(0);
+	let count = 1;
+	for(i=1 ; i<str.length ; i++){
+		if(a!=str.chartAt(i) && count == 1)
+		{
+			return a;
+		}
+		else if(a!=str.charAt(i) && count != 1)
+		{
+			a=str.charAt(i);
+			count = 1;
+		}
+		else
+		{
+			count++;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
